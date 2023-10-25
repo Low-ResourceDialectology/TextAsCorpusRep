f07b9c69c9fb19dce0b39103bcd3e9050b93e9e4
Christian Schuler <christianschuler8989@gmail.com>
2023-10-25 11:34:20 +0200

    Update the kurmanji preprocessing

--- scr/preprocess_kurmanji.py
+++ scr/preprocess_kurmanji.py
@@ -11,6 +11,8 @@ import json
 import os
 from bs4 import BeautifulSoup
 from lxml import etree
+import glob # For reading multiple txt files and write them into a single file in one go
+# https://stackoverflow.com/questions/17749058/combine-multiple-text-files-into-one-text-file-using-python 
 
 path_kurmanji = "./../data/Kurmanji/"
 
@@ -29,78 +31,145 @@ with open(path_kurmanji+'Temp/2023AhmadiSouthernCorpus-kur.txt', 'w') as file:
     for line in input_lines:
         file.write(line)
 
+# We ignore the arabic script for now, since it is barely used for Kurmanji and usually Sorani (Central Kurdish)
 # Read the .txt file line-by-line
-with open(path_kurmanji+'Datasets/2023AhmadiSouthernCorpus/KurdishLID/datasets/NorthernKurdish-arab_train.txt') as file:
-    input_lines = file.readlines()
-
+#with open(path_kurmanji+'Datasets/2023AhmadiSouthernCorpus/KurdishLID/datasets/NorthernKurdish-arab_train.txt') as file:
+#    input_lines = file.readlines()
 # Write the text into a new file
-with open(path_kurmanji+'Temp/2023AhmadiSouthernCorpus-kur_arab.txt', 'w') as file:
-    for line in input_lines:
-        file.write(line)
+#with open(path_kurmanji+'Temp/2023AhmadiSouthernCorpus-kur_arab.txt', 'w') as file:
+#    for line in input_lines:
+#        file.write(line)
 
 # 2022AhmadiInterdialect 
 ###############################################################################
+# Kurmanji - English
+####################
 input_lines = []
 
-# Okay... Reading xml files that are as basic as this one and which also contain duplicated ids is a pain in the butt!
-"""
-tree = etree.parse(path_kurmanji+'Datasets/2022AhmadiInterdialect/InterdialectCorpus/KMR-ENG/KMR-ENG.kmr.eng.xml')
-for graph in tree.xpath('//p'):
-    print(graph.xpath('@id')[0])
-
-root = tree.getroot()
-
-topic = root.find(".//*[@id='1:2']").text
-print(topic)
-"""
-
-"""
-# Reading the data inside the xml
-# file to a variable under the name 
-# data
-with open(path_kurmanji+'Datasets/2022AhmadiInterdialect/InterdialectCorpus/KMR-ENG/KMR-ENG.kmr.eng.xml', 'r') as f:
-    data = f.read()
- 
-# Passing the stored data inside
-# the beautifulsoup parser, storing
-# the returned object 
-Bs_data = BeautifulSoup(data, "xml")
- 
-# Finding all instances of tag 
-# `unique`
-b_unique = Bs_data.find_all('unique')
- 
-print(b_unique)
- 
-print(value)
-"""
-
-
 # Read the .txt file line-by-line
-with open(path_kurmanji+'Datasets/2022AhmadiInterdialect/InterdialectCorpus/KMR-ENG/KMR-ENG.kmr.eng.xml') as file:
+with open(path_kurmanji+'Datasets/2022AhmadiInterdialect/InterdialectCorpus/KMR-ENG/KMR-ENG.KMR_no_tag.txt') as file:
     input_lines = file.readlines()
 
-# input_lines has this format: 
-# Start of file:       <p id="1"><s id="1:1">Hunermendên Kurd wê ji bo yekîtiya neteweyî li Stockholmê konserekê li dar bixin.</s>	<s id="1:1">A joint concert of numerous Kurdish musicians took place in Stockholm.</s>
-# And each line contains 2 sentences like this:         <s id="1:2">Hunermendên Kurd ji bo tevkariyê li pêkhatina yekîtiya neteweyî bikin, wê konserekê li dar bixin.</s>	<s id="1:2">The artists want to contribute to Kurdish national unity.</s>
+# Write the text into a new file
+with open(path_kurmanji+'Temp/2022AhmadiInterdialect-kur_eng.txt', 'w') as file:
+    for line in input_lines:
+        file.write(line)
 
-# Cleaning up the lines to extract the Kurmanji and English texts
-#for each line in input_lines:
-    
+input_lines = []
 
-
-data = input_lines
+# Read the .txt file line-by-line
+with open(path_kurmanji+'Datasets/2022AhmadiInterdialect/InterdialectCorpus/KMR-ENG/KMR-ENG.ENG_no_tag.txt') as file:
+    input_lines = file.readlines()
 
 # Write the text into a new file
-with open(path_kurmanji+'Temp/2022AhmadiInterdialect-kur-eng.txt', 'w') as file:
-    for line in data:
+with open(path_kurmanji+'Temp/2022AhmadiInterdialect-eng_kur.txt', 'w') as file:
+    for line in input_lines:
         file.write(line)
 
 
-
-# 2023AhmadiSouthernCorpus 
+# 2020AhmadiKurdishTokenization
 ###############################################################################
+# Kurmanji
+##########
+input_lines = []
 
+# Read the .txt file line-by-line
+with open(path_kurmanji+'Datasets/2020AhmadiKurdishTokenization/KurdishTokenization/data/kmr_sentences.txt') as file:
+    input_lines = file.readlines()
 
-# 2023AhmadiSouthernCorpus 
+# Write the text into a new file
+with open(path_kurmanji+'Temp/2020AhmadiKurdishTokenization-kur.txt', 'w') as file:
+    for line in input_lines:
+        file.write(line)
+
+# 2013EsmailiPewan
 ###############################################################################
+# Kurmanji
+##########
+read_files = glob.glob(path_kurmanji+'Datasets/2013EsmailiPewan/pewan/Pewan/Corpora/Kurmanji/docs2/'+"*.txt")
+
+# Write the text into a new file
+with open(path_kurmanji+'Temp/2013EsmailiPewan-kur.txt', 'w') as outfile:
+    for file in read_files:
+        with open(file, "r") as infile:
+            outfile.write(infile.read())
+            outfile.write('\n')
+
+# 2020FatihkurtKurdishTwitter
+###############################################################################
+# Kurmanji
+##########
+input_lines = []
+
+# Read the .txt file line-by-line
+with open(path_kurmanji+'Datasets/2020FatihkurtKurdishTwitter/kurdish-twitter-data/Kurmanji/twitter-data.txt') as file:
+    input_lines = file.readlines()
+
+# Write the text into a new file
+with open(path_kurmanji+'Temp/2020FatihkurtKurdishTwitter-kur.txt', 'w') as file:
+    for line in input_lines:
+        file.write(line)
+
+# 2020LeichtfußFreeDict
+###############################################################################
+# German-Kurmanji
+#################
+input_lines = []
+
+# Read the .txt file line-by-line
+with open(path_kurmanji+'Datasets/2020LeichtfußFreeDict/fd-dictionaries/deu-kur/Deutsch-Kurdî.txt') as file:
+    input_lines = file.readlines()
+
+# Write the text into a new file
+with open(path_kurmanji+'Temp/2020LeichtfußFreeDict-ger_kur.txt', 'w') as file:
+    for line in input_lines:
+        file.write(line)
+
+# Kurmanji-German
+#################
+# Read the .txt file line-by-line
+with open(path_kurmanji+'Datasets/2020LeichtfußFreeDict/fd-dictionaries/kur-deu/kur-deu.txt') as file:
+    input_lines = file.readlines()
+
+# Write the text into a new file
+with open(path_kurmanji+'Temp/2020LeichtfußFreeDict-kur_ger.txt', 'w') as file:
+    for line in input_lines:
+        file.write(line)
+
+# Kurmanji-English
+##################
+# Read the .txt file line-by-line
+with open(path_kurmanji+'Datasets/2020LeichtfußFreeDict/fd-dictionaries/kur-eng/kur-eng.txt') as file:
+    input_lines = file.readlines()
+
+# Write the text into a new file
+with open(path_kurmanji+'Temp/2020LeichtfußFreeDict-kur_eng.txt', 'w') as file:
+    for line in input_lines:
+        file.write(line)
+
+# Kurmanji-Turkish
+##################
+# Read the .txt file line-by-line
+with open(path_kurmanji+'Datasets/2020LeichtfußFreeDict/fd-dictionaries/kur-tur/kur-tur.txt') as file:
+    input_lines = file.readlines()
+
+# Write the text into a new file
+with open(path_kurmanji+'Temp/2020LeichtfußFreeDict-kur_tur.txt', 'w') as file:
+    for line in input_lines:
+        file.write(line)
+
+
+# 2001HaigKurdishNewspaper
+###############################################################################
+# Kurmanji
+##########
+input_lines = []
+
+# Read the .txt file line-by-line
+with open(path_kurmanji+'Datasets/2001HaigKurdishNewspaper/ccknt.txt', encoding="latin-1") as file:
+    input_lines = file.readlines()#.decode("latin-1")
+
+# Write the text into a new file
+with open(path_kurmanji+'Temp/2001HaigKurdishNewspaper-kur.txt', 'w', encoding="UTF-8") as file:
+    for line in input_lines:
+        file.write(line)#.encode("UTF-8")
